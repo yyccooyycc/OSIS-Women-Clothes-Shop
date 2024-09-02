09fc95829ed834b9a2f297830d87d62425736fd1
Olivia Huang <olivia.huang@sunbirddcim.com>
2024-09-02 17:23:43 +0800

    update dropdown expansion to 50vw and rotate the menu button

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -34,24 +34,9 @@ const DropdownMenu = () => {
   return (
     <li className="nav-item" ref={dropdownRef}>
       <div className="dropdown">
-        <button
-          className="dropdown-toggle"
-          onClick={() => dispatch(toggle())}
-          type="button"
-          id="menuDropdown"
-          aria-haspopup="true"
-          aria-expanded={isOpen ? "true" : "false"}
-          style={{
-            background: "transparent",
-            border: "none",
-            padding: 0,
-            cursor: "pointer",
-            display: "flex",
-            alignItems: "center",
-            pointerEvents: "auto",
-          }}
-        >
+ 
           <svg
+            className="dropdown-icon"
             clipRule="evenodd"
             fillRule="evenodd"
             strokeLinejoin="round"
@@ -59,17 +44,27 @@ const DropdownMenu = () => {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
             style={{ width: "24px", height: "24px", fill: "white" }}
+            onClick={() => dispatch(toggle())}
           >
             <path
               d="m11.6 11c0-.552-.448-1-1-1-1.655 0-4.945 0-6.6 0-.552 0-1 .448-1 1v9c0 .552.448 1 1 1h6.6c.552 0 1-.448 1-1 0-2.092 0-6.908 0-9zm9.4 6c0-.552-.448-1-1-1h-6c-.538 0-1 .477-1 1v3c0 .552.448 1 1 1h6c.552 0 1-.448 1-1zm0-13c0-.552-.448-1-1-1-1.537 0-4.463 0-6 0-.552 0-1 .448-1 1v9.6c0 .552.448 1 1 1h6c.552 0 1-.448 1-1 0-2.194 0-7.406 0-9.6zm-9.4 0c0-.552-.448-1-1-1-1.655 0-4.945 0-6.6 0-.552 0-1 .448-1 1v3.6c0 .552.448 1 1 1h6.6c.552 0 1-.448 1-1 0-1.017 0-2.583 0-3.6z"
               fillRule="nonzero"
             />
           </svg>
-        </button>
       </div>
       <div
         className={`dropdown-menu ${isOpen ? "show" : ""}`}
-        style={{ position: "unset" }}
+        style={{
+          position: "fixed",
+          padding: "60px",
+          top: 0,
+          left: 0,
+          width: "70vw",
+          height: "100vh",
+          backgroundColor: "#DFD3C3",
+          zIndex: 1000,
+          display: isOpen ? "block" : "none",
+        }}
       >
         {isOpen && (
           <ul>
