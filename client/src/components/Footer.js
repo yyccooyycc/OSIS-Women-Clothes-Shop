1ba2d0a4f1ca6a003d7b8b3a1f351222eaae8b9c
Olivia Huang <olivia.huang@sunbirddcim.com>
2024-09-20 17:20:50 +0800

    fix Received "true" for a non-boolean attribute error for item

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,14 +12,14 @@ const Footer = () => {
       }}
     >
       <Grid2 container spacing={2}>
-        <Grid2 item xs={12} sm={4}>
+        <Grid2 item="true" xs={12} sm={4}>
           <Typography variant="h6"> OSIS Women Clothings</Typography>
           <Typography variant="body2">
             © {new Date().getFullYear()} OSIS Women Clothings. All rights reserved.
           </Typography>
         </Grid2>
         
-        <Grid2 item xs={12} sm={4}>
+        <Grid2 item="true" xs={12} sm={4}>
           <Typography variant="h6">Quick Links</Typography>
           <Link href="#" color="inherit" underline="hover">About Us</Link><br />
           <Link href="#" color="inherit" underline="hover">Shipping</Link><br />
@@ -27,7 +27,7 @@ const Footer = () => {
           <Link href="#" color="inherit" underline="hover">Privacy Policy</Link>
         </Grid2>
 
-        <Grid2 item xs={12} sm={4}>
+        <Grid2 item="true" xs={12} sm={4}>
           <Typography variant="h6">Contact Us</Typography>
           <Typography variant="body2">Email: ooo@osis-women-clothings.com</Typography>
           <Typography variant="body2">Phone: +02-1234-5678</Typography>
