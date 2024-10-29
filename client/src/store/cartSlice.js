417d0ea118cebcda1322f69dafe522e90f89e1f4
Olivia Huang <olivia.huang@sunbirddcim.com>
2024-10-29 16:58:23 +0800

    update the item counts for shoppoing cart icon

--- client/src/store/cartSlice.js
+++ client/src/store/cartSlice.js
@@ -13,20 +13,20 @@ const cartSlice= createSlice({
             const itemIndex=state.cart.findIndex(item=>item.id===action.payload.id);
             if(itemIndex>=0){
                 state.cart[itemIndex].quantity+=1;
-                state.totalItemCount+=1;
             }else{
                 state.cart.push({...action.payload,quantity:1});
             }
+            state.totalItemCount+=1;
         },
         removeFromCart: (state, action) => {
             const itemIndex=state.cart.findIndex(item=>item.id===action.payload.id);
             if(itemIndex>=0){
                 if(state.cart[itemIndex].quantity>1){
                     state.cart[itemIndex].quantity-=1;
-                    state.totalItemCount-=1;
                 }else{
-                state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+                    state.cart = state.cart.filter((item) => item.id !== action.payload.id);
                 }
+                state.totalItemCount-=1;
             }
         },
     }
